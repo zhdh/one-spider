9011bf1db476e0c5c68ddeeef803fac0107d5edf
Zhang Donghan <944369375@QQ.COM>
2018-04-13 14:00:44 +0800

    init spider

--- src/main/java/com/one/spider/OneStoryPageProcessor.java
+++ src/main/java/com/one/spider/OneStoryPageProcessor.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
@@ -13,19 +14,22 @@ import us.codecraft.webmagic.processor.PageProcessor;
  */
 public class OneStoryPageProcessor implements PageProcessor{
 
-    Logger log = LoggerFactory.getLogger(OneStoryPageProcessor.class);
+    private Logger log = LoggerFactory.getLogger(OneStoryPageProcessor.class);
+
+    private Site site = Site.me().setTimeOut(20000).setRetryTimes(3).setSleepTime(3000).setRetrySleepTime(3000);
 
     @Override
     public void process(Page page) {
-        log.info(page.getHtml().toString());
+        log.info("content: " + page.getHtml().toString());
     }
 
     @Override
     public Site getSite() {
-        return null;
+        return site;
     }
 
     public static void main(String[] args){
-
+        String url = "http://wufazhuce.com/";
+        Spider.create(new OneStoryPageProcessor()).addUrl(url).thread(1).run();
     }
 }
