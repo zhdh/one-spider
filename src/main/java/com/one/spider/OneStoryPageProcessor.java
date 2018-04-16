ed1d3f067b4e76e8835efe21e2a82a637350de93
Zhang Donghan <944369375@QQ.COM>
2018-04-16 19:57:06 +0800

    get some elements and update log properties

--- src/main/java/com/one/spider/OneStoryPageProcessor.java
+++ src/main/java/com/one/spider/OneStoryPageProcessor.java
@@ -28,9 +28,15 @@ public class OneStoryPageProcessor implements PageProcessor{
         String day = html.xpath("//*[@id=\"carousel-one\"]/div/div[1]/div[2]/div[1]/p[2]/text()").toString().trim();
         String word = html.xpath("//*[@id=\"carousel-one\"]/div/div[1]/div[2]/div[2]/a/text()").toString().trim();
         String imageUrl = html.xpath("//*[@id=\"carousel-one\"]/div/div[1]/a").$("img","src").toString().trim();
-        log.info("type: " + type + " date: "+ date + " " + day + " word: " + word );
+        String articleTitle = html.xpath("//*[@id=\"main-container\"]/div[1]/div[2]/div/div/div[1]/div/p[2]/a/text()").toString().trim();
+        String articleURL = html.xpath("//*[@id=\"main-container\"]/div[1]/div[2]/div/div/div[1]/div/p[2]/").$("a","href").toString().trim();
+        String questionURL = html.xpath("//*[@id=\"main-container\"]/div[1]/div[2]/div/div/div[2]/div/p[2]/").$("a","href").toString().trim();
+        String questionTitle = html.xpath("//*[@id=\"main-container\"]/div[1]/div[2]/div/div/div[2]/div/p[2]/a/text()").toString().trim();
         String imageName = imageUrl.split("/")[3] + ".jpg";
         String outputPath = System.getProperty("user.dir") + "\\image";
+        log.info("type: " + type + " date: "+ date + " " + day + " word: " + word );
+        log.info("article: " + articleTitle + " " + articleURL);
+        log.info("questionURL: " + questionTitle + " " + questionURL);
     }
 
     @Override
