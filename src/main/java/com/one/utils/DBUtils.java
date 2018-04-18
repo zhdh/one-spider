1110743e5a8b8a77b40aede847a251dbf53a172a
Zhang Donghan <944369375@QQ.COM>
2018-04-18 20:29:58 +0800

    add DBUtils and create 'data' database

--- src/main/java/com/one/utils/DBUtils.java
+++ src/main/java/com/one/utils/DBUtils.java
@@ -1,4 +1,23 @@
 package com.one.utils;
 
+import java.sql.*;
+
+/**
+ * SQLite utils
+ * @author https://github.com/zhdh
+ */
 public class DBUtils {
+
+    public static void createTable() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:db/data.db");
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS t_one");
+        stmt.executeUpdate("CREATE TABLE t_one (id INTEGER AUTO_INCREMENT PRIMARY KEY ,type STRING,date STRING,word STRING,imageURL STRING,articleTitle STRING, articleAbstract STRING,articleAuthor STRING,articleContent STRING,questionTitle STRING,questionAbstract STRING,questionConetnt STRING, insertTime TIMESTAMP default CURRENT_TIMESTAMP )");
+        stmt.close();
+        conn.close();
+    }
+
+
+
 }
